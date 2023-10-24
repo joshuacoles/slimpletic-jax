ee5ff32e51537ed3515762d4e9a25df68421d453
Joshua Coles <josh@coles.to>
2023-10-24 10:42:30 +0100

    AND TAKES ADDITIONAL ARGS

--- sd.py
+++ sd.py
@@ -3,8 +3,8 @@ from jaxopt import GaussNewton
 from jax import numpy as jnp
 
 
-def rosenbrock(x):
-    a = x[0]
+def rosenbrock(x, additional_arg):
+    a = additional_arg * x[0]
     b = x[1]
 
     return jnp.array([
@@ -13,6 +13,13 @@ def rosenbrock(x):
     ])
 
 
+additional_arg_value = 10.0
+
 gn = GaussNewton(residual_fun=rosenbrock)
-gn_sol = gn.run(jnp.array([1.0, 2.0])).params
-jax.debug.print("sol = {} with f(sol) = {}", gn_sol, rosenbrock(gn_sol))
+gn_sol = gn.run(jnp.array([1.0, 2.0]), additional_arg=additional_arg_value).params
+jax.debug.print(
+    "sol = {} with f(sol, {}) = {}",
+    gn_sol,
+    additional_arg_value,
+    rosenbrock(gn_sol, additional_arg=additional_arg_value)
+)
