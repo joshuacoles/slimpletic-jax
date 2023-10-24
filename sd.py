9fdf0d5cc37977d5494f785981c81fd0de4af8d5
Joshua Coles <josh@coles.to>
2023-10-24 11:12:12 +0100

    Pain

--- sd.py
+++ sd.py
@@ -3,7 +3,7 @@ import jaxopt
 from jax import numpy as jnp
 
 
-def system(x, additional_arg):
+def o_system(x, additional_arg):
     a = additional_arg * x[0]
     b = x[1]
 
@@ -13,13 +13,38 @@ def system(x, additional_arg):
     ])
 
 
-additional_arg_value = 10.0
+def form_system(
+        r: int
+):
+    def system(
+            qi_n,
+            q_n,
+            pi_n,
+            t_n,
+            dt,
+            r
+    ):
+        a = qi_n[0]
+        b = qi_n[1]
+
+        return jnp.array([
+            10 * (6 * b ** 4 - a ** 2),
+            1 + 2 * a
+        ])
+
+    return system
+
+
+system = form_system(
+    r=2
+)
 
 gn = jaxopt.GaussNewton(residual_fun=system)
-gn_sol = gn.run(jnp.array([1.0, 2.0]), additional_arg=additional_arg_value).params
+gn_sol = gn.run(
+    jnp.array([1.0, 2.0])
+).params
 jax.debug.print(
-    "sol = {} with f(sol, {}) = {}",
+    "sol = {} with f(sol) = {}",
     gn_sol,
-    additional_arg_value,
-    system(gn_sol, additional_arg=additional_arg_value)
+    system(gn_sol)
 )
