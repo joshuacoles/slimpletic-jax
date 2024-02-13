1755f0b85438a937253874706af2d9f0a51eb1d2
Joshua Coles <josh@coles.to>
2024-02-13 16:22:33 +0000

    WE HAVE FOUND THE ERROR LOCATION

--- test_cases/harness_1.py
+++ test_cases/harness_1.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from harness import original, solver, lagrangian_f, L, r, dt
 from jax import numpy as jnp, config as jax_config
 import numpy as np
@@ -8,14 +6,6 @@ assert jax_config.read('jax_enable_x64')
 
 
 # The first thing to check is that the lagrangians are actually the same
-
-def eval_sympy_lagrangian(q: jnp.ndarray, v: jnp.ndarray, t: float):
-    return float(L.subs([
-        *zip(original.q, q),
-        *zip(original.v, v),
-        (original.t, t)
-    ]))
-
 
 dof = original.degrees_of_freedom
 
@@ -45,7 +35,6 @@ for _ in range(100):
 print("Success")
 
 print("Testing pi_next")
-# The next thing is to test the discretised lagrangians are the same
 for _ in range(100):
     qi_values = jnp.array(np.random.normal(size=(r + 2, dof)))
     t = np.random.rand()
@@ -54,4 +43,26 @@ for _ in range(100):
         original.compute_pi_next(qi_values, t, dt),
         solver.compute_pi_next(qi_values, t)
     )
+print("Success")
+
+print("Testing compute_qi_values")
+for _ in range(100):
+    previous_q = jnp.array(np.random.rand(dof))
+    previous_pi = jnp.array(np.random.rand(dof))
+    t = np.random.rand()
+
+    original_values = original.compute_qi_values(previous_q, previous_pi, t, dt)
+    jax_values = solver.compute_qi_values(previous_q, previous_pi, t)
+
+    try:
+        assert jnp.allclose(
+            original_values,
+            jax_values
+        )
+    except AssertionError:
+        print(original_values)
+        print(jax_values)
+        print(original_values - jax_values)
+        raise
+
 print("Success")
