656f8110d085c0727795b3509c98952e2ff4719e
Joshua Coles <josh@coles.to>
2024-02-19 20:55:18 +0000

    Feed embedding action into the solver

--- loss_fn/__init__.py
+++ loss_fn/__init__.py
@@ -2,6 +2,7 @@ import jax.lax
 import jax.numpy as jnp
 import numpy as np
 from jax import jit, grad
+from slimpletic import Solver
 
 
 @jit
@@ -14,5 +15,23 @@ def compute_action(state, embedding):
     )
 
 
-print(compute_action(jnp.array(np.random.rand(3)), jnp.array(np.random.rand(9))))
-print(grad(compute_action, argnums=(1,))(jnp.array(np.random.rand(3)), jnp.array(np.random.rand(9))))
+# print(compute_action(jnp.array(np.random.rand(3)), jnp.array(np.random.rand(9))))
+# print(grad(compute_action, argnums=(1,))(jnp.array(np.random.rand(3)), jnp.array(np.random.rand(9))))
+
+embedding = jnp.array(np.random.rand(9)) * 10
+
+print(embedding)
+
+solver = Solver(
+    dt=0.1,
+    r=0,
+    lagrangian=jit(lambda q, v, t: compute_action(jnp.concat([q, v], axis=0), embedding)),
+    k_potential=None
+)
+
+print(solver.integrate(
+    q0=jnp.array([1.0, 0.0, 0.0]),
+    pi0=jnp.array([0.0, 0.0, 0.0]),
+    t0=0.0,
+    iterations=10
+))
