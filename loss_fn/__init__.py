ed2575267f01fa6bcf3f6787313339e318601685
Joshua Coles <josh@coles.to>
2024-02-20 13:41:50 +0000

    Work from last night, need to consider what the interface needed for two different cases. Predicting one model forwards from lots of different initial conditions, vs predicting lots of models forward from one initial condition

--- loss_fn/__init__.py
+++ loss_fn/__init__.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import jax.lax
 import jax.numpy as jnp
 import numpy as np
@@ -20,18 +22,65 @@ def compute_action(state, embedding):
 
 embedding = jnp.array(np.random.rand(9)) * 10
 
-print(embedding)
+# print(embedding)
 
-solver = Solver(
+# solver = Solver(
+#     dt=0.1,
+#     r=0,
+#     lagrangian=jit(lambda q, v, t: compute_action(jnp.concat([q, v], axis=0), embedding)),
+#     k_potential=None
+# )
+#
+# print(solver.integrate(
+#     q0=jnp.array([1.0, 0.0, 0.0]),
+#     pi0=jnp.array([0.0, 0.0, 0.0]),
+#     t0=0.0,
+#     iterations=10
+# ))
+
+
+def loss_fn(embedding: jnp.ndarray, target_q: jnp.ndarray, target_pi: jnp.ndarray):
+    assert target_q.size == target_pi.size
+
+    solver = Solver(
+        dt=0.1,
+        r=0,
+        lagrangian=jit(lambda q, v, t: compute_action(jnp.concat([q, v], axis=0), embedding)),
+        k_potential=None
+    )
+
+    q, pi = solver.integrate(
+        q0=jnp.array([1.0, 0.0, 0.0]),
+        pi0=jnp.array([0.0, 0.0, 0.0]),
+        t0=0,
+        iterations=10
+    )
+
+    return jnp.sqrt(jnp.mean(jnp.abs(q - target_q) ** 2) + jnp.mean(jnp.abs(pi - target_pi) ** 2))
+
+
+expected_system = Solver(
     dt=0.1,
     r=0,
-    lagrangian=jit(lambda q, v, t: compute_action(jnp.concat([q, v], axis=0), embedding)),
+    lagrangian=lambda q, v, t: jnp.dot(q, q) + jnp.dot(v, v),
     k_potential=None
 )
 
-print(solver.integrate(
+exptected_q, expected_pi = expected_system.integrate(
     q0=jnp.array([1.0, 0.0, 0.0]),
     pi0=jnp.array([0.0, 0.0, 0.0]),
     t0=0.0,
     iterations=10
+)
+
+print(grad(loss_fn, argnums=(0,))(
+    jnp.array(np.random.rand(9)),
+    exptected_q,
+    expected_pi
 ))
+
+# loss_fn(
+#     jnp.array(np.random.rand(9)),
+#     exptected_q,
+#     expected_pi
+# )
