68b50a3e8b75214d79e1401db26df03f6f4e8898
Aengus Roberts <asr66@bath.ac.uk>
2024-03-28 10:09:16 +0000

    neatening

--- aengus/test_MMVP.py
+++ aengus/test_MMVP.py
@@ -18,18 +18,21 @@ TIMESTEPS = 40
 
 
 def custom_loss(y_True, y_Pred):
-    print(y_Pred.numpy)
-    q_true, q_pred = [], []
-    qP = slimplecticSoln(TIMESTEPS, False, 1, y_Pred)[0]
-    qT = slimplecticSoln(TIMESTEPS, False, 1, y_True)[0]
-    q_pred.append(qP[0])
-    q_true.append(qT[0])
+    ypnp = y_Pred.numpy()
+    ytnp = y_True.numpy()
+    print(ypnp[0])
+    print(y_True.shape)
+    totalLoss = 0
 
-    loss = 0
-    for i in enumerate(qT):
-        loss += (qT[i] - qP[i]) ** 2
+    for index in range(0, ypnp.shape[0]):
+        qP = slimplecticSoln(TIMESTEPS, ypnp[index])[0]
+        qT = slimplecticSoln(TIMESTEPS, ytnp[index])[0]
 
-    return loss ** 0.5
+        # RMS of the true vs predicted q values
+        totalLoss += np.sqrt(np.sum((qT - qP) ** 2))
+
+    print(f"ALPHA Returning loss {totalLoss}")
+    return totalLoss / (ypnp.shape[0])
 
 
 def loadData(XData, YData):
@@ -61,43 +64,53 @@ def loadData(XData, YData):
 X, Y, timestep_filter, datasize_filter = loadData(XName, YName)
 
 
-def create_layer(units, regularizer):
-    return tf.keras.layers.LSTM(units=50 * TRAINING_TIMESTEPS, input_shape=(TRAINING_TIMESTEPS + 1, 2),
-                                return_sequences=True,
-                                kernel_regularizer=tf.keras.regularizers.L1L2())
+def create_layer(units, regularizer, i):
+    if regularizer[i] == 1:
+        return tf.keras.layers.LSTM(units=units[i], input_shape=(TRAINING_TIMESTEPS + 1, 2),
+                                    return_sequences=True,
+                                    kernel_regularizer=tf.keras.regularizers.L1L2())
+    else:
+        return tf.keras.layers.LSTM(units=units[i], input_shape=(TRAINING_TIMESTEPS + 1, 2),
+                                    return_sequences=True)
 
 
-if __name__ == "__main__":
+def runModel(layers: int, units: list, regulariser: list, dropout: float, batchsize: int):
+    """
+    :param layers: int identifying the number of layers
+    :param units: list indexing how many units each layer has
+    :param regulariser: list indexing which layers have regularisers
+    :param dropout: float in [0.05,0.1,0.2,0.3,0.4]
+    :param batchsize: 64,128,256 are usual values
+    :return: Loss and ValLoss Lists indexed by Epoch
+    """
+
     # Model Definition
     model = tf.keras.Sequential([
-        tf.keras.layers.LSTM(units=50 * TRAINING_TIMESTEPS, input_shape=(TRAINING_TIMESTEPS + 1, 2),
-                             return_sequences=True,
-                             kernel_regularizer=tf.keras.regularizers.L1L2()),
-        tf.keras.layers.LSTM(units=25 * TRAINING_TIMESTEPS, input_shape=(TRAINING_TIMESTEPS + 1, 2),
-                             return_sequences=True,
-                             kernel_regularizer=tf.keras.regularizers.L1L2()),
-        tf.keras.layers.LSTM(units=10 * TRAINING_TIMESTEPS, input_shape=(TRAINING_TIMESTEPS + 1, 2),
-                             return_sequences=True,
-                             kernel_regularizer=tf.keras.regularizers.L1L2()),
-        tf.keras.layers.LSTM(units=5 * TRAINING_TIMESTEPS, input_shape=(TRAINING_TIMESTEPS + 1, 2),
-                             return_sequences=True, kernel_regularizer=tf.keras.regularizers.L1L2()),
-        # Optional Other Layers HERE
-        tf.keras.layers.Dropout(0.05),
+        *[create_layer(units, regulariser, i + 4 - layers) for i in range(layers)],
+        tf.keras.layers.Dropout(dropout),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(units=4)
     ])
 
     # Compile the model
     # 'mean_squared_error'
-    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.compile(optimizer='adam', loss=custom_loss, run_eagerly=True)
 
     # Train the model
-    model_loss = model.fit(X, Y, epochs=EPOCHS, batch_size=128, validation_split=0.2, verbose=2)
+    model_loss = model.fit(X, Y, epochs=EPOCHS, batch_size=batchsize, validation_split=0.2, verbose=2)
+
+    # return loss
+    loss_list = model_loss.history["loss"]
+    val_loss_list = model_loss.history["val_loss"]
+
+    return loss_list, val_loss_list
+
+
+if __name__ == "__main__":
+    loss_list, val_loss_list = runModel(1, [5, 5, 5, 5], [1, 1, 1, 1], 0.2, 128)
 
     # Make Plot of Loss
     title = dataName + ", " + "datapoints: " + str(timestep_filter) + ", Datasize: " + str(datasize_filter)
-    loss_list = model_loss.history["loss"]
-    val_loss_list = model_loss.history["val_loss"]
     epochs = [i for i in range(1, EPOCHS + 1)]
     plt.plot(epochs, loss_list)
     plt.plot(epochs, val_loss_list)
