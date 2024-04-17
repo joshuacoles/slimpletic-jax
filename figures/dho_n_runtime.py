f09b578bf80439ccddb7e3aedec2778fd58eafdc
Joshua Coles <josh@coles.to>
2024-04-17 13:43:37 +0100

    Improve dho_n_runtime

--- figures/dho_n_runtime.py
+++ figures/dho_n_runtime.py
@@ -6,9 +6,8 @@ from neural_networks.data import project_data_root
 import matplotlib.pyplot as plt
 import numpy as np
 import jax.numpy as jnp
-import pandas
 
-from neural_networks.data.families import Family, dho
+from neural_networks.data.families import dho
 from slimpletic import SolverScan, DiscretisedSystem, GGLBundle
 
 r = 2
@@ -24,6 +23,13 @@ solver = SolverScan(DiscretisedSystem(
     pass_additional_data=True
 ))
 
+current_datetime = datetime.datetime.now().isoformat()
+output_dir = project_data_root.joinpath('figures', 'dho_n_runtime', current_datetime)
+output_dir.mkdir(exist_ok=True, parents=True)
+
+fig_out = output_dir.joinpath('figure.png')
+csv_out = open(output_dir.joinpath('data.csv'), "w")
+
 # Run the system once to warm up the JIT
 embedding = jnp.array([1.0, 1.0, 1.0])
 
@@ -32,57 +38,44 @@ def sample_jax(iterations: int):
     print(f"Running JAX with {iterations}")
 
     start = time.time()
-    solver.integrate(
-        q0=jnp.array([1.0]),
-        pi0=jnp.array([1.0]),
-        t0=0,
-        iterations=iterations,
-        additional_data=embedding
-    )
-    end = time.time()
-    return end - start
+    repeat_samples = 4
 
+    for _ in range(repeat_samples):
+        solver.integrate(
+            q0=jnp.array([1.0]),
+            pi0=jnp.array([1.0]),
+            t0=0,
+            iterations=iterations,
+            additional_data=embedding
+        )
 
-def sample_original(iterations: int):
-    start = time.time()
-    dho.solve(
-        embedding,
-        jnp.array([1.0]), jnp.array([1.0])
-    )
     end = time.time()
-    return end - start
+    end_start = (end - start) / repeat_samples
+
+    # Write data eagerly to save stuff for if we have to early exit
+    csv_out.write(f"{iterations},{end_start}\n")
+    csv_out.flush()
+    return end_start
 
 
 # Warm up the JIT
 sample_jax(5)
 
 # iteration_points = np.linspace(10, 1000, 100, dtype=int)
-iteration_points = 10 ** np.arange(0, 7, 0.25)
+iteration_points = 10 ** np.arange(0, 7, 0.1)
 times = np.array([sample_jax(int(n)) for n in iteration_points])
 
-df = pandas.DataFrame({
-    "iterations": iteration_points,
-    "times": times
-})
-
-project_data_root.joinpath('figures').mkdir(
-    exist_ok=True,
-    parents=True
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+line, = ax.plot(
+    iteration_points,
+    times,
 )
 
-current_datetime = datetime.datetime.now().isoformat()
+ax.set_xlabel("Number of iterations")
+ax.set_ylabel("Total integration time / s")
 
-df.to_csv(project_data_root.joinpath(
-    'figures',
-    f"{current_datetime}.csv"
-))
+ax.set_xscale('log')
+fig.savefig(fig_out)
 
-plt.plot(iteration_points, times)
-plt.xlabel("Number of iterations")
-plt.ylabel("Total integration time / s")
-
-plt.savefig(project_data_root.joinpath(
-    'figures',
-    f"{current_datetime}.png"
-))
 plt.show()
