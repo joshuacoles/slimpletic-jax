f0a18244421118a449f1eaf1e2092f4ac4b721e5
Joshua Coles <josh@coles.to>
2024-04-20 15:00:31 +0100

    Figures

--- figures/dho_n_runtime/make_graph.py
+++ figures/dho_n_runtime/make_graph.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-jax = pd.read_csv("../data/dho_n_t/jax.csv")
-original = pd.read_csv("../data/dho_n_t/original.csv")
+from neural_networks.data import project_data_root
+
+jax = pd.read_csv(project_data_root / "dho_n_t/jax.csv")
+original = pd.read_csv(project_data_root / "dho_n_t/original.csv")
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -31,4 +33,4 @@ ax.set_xscale('log')
 ax.set_yscale('log')
 fig.legend()
 
-plt.show()
+plt.savefig(project_data_root / "figures" / "dho_n_runtime" / "figure.png")
